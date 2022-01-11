d36a40c7e8bf7beafac286c042d71d7fd0e061eb
haneen jaradeh <haneenjaradeh1@gmail.com>
2022-01-11 11:05:32 +0200

    task2

--- app.js
+++ app.js
@@ -6,3 +6,46 @@ var nextStep = document.querySelector('#nextStep');
     document.getElementById('thank_you').style.display = 'block';
 
   });
+  function myFunction() {
+    document.getElementById("demo").style.fontSize = "xx-large";
+  }
+
+  function bigImg(x) {
+    x.style.fontSize = "30px";
+    
+}
+function normalImg(x) {
+  x.style.fontSize = "17px";
+}
+
+
+
+  function afterr(){
+    document.getElementById('myImage').src="https://media.istockphoto.com/vectors/checkmark-vector-icon-put-a-green-symbol-on-the-white-background-vector-id1151872207?k=20&m=1151872207&s=170667a&w=0&h=NzKOi1fjRT-jYlohJ7a15LRWHC3wK0LjTu1fi9yOQYQ=";
+
+}
+function afterr1(){
+  document.getElementById('image1').src="https://media.istockphoto.com/vectors/checkmark-vector-icon-put-a-green-symbol-on-the-white-background-vector-id1151872207?k=20&m=1151872207&s=170667a&w=0&h=NzKOi1fjRT-jYlohJ7a15LRWHC3wK0LjTu1fi9yOQYQ=";
+
+
+}
+function afterr2(){
+  document.getElementById('image2').src="https://media.istockphoto.com/vectors/checkmark-vector-icon-put-a-green-symbol-on-the-white-background-vector-id1151872207?k=20&m=1151872207&s=170667a&w=0&h=NzKOi1fjRT-jYlohJ7a15LRWHC3wK0LjTu1fi9yOQYQ=";
+
+
+}
+function afterr3(){
+  document.getElementById('image3').src="https://media.istockphoto.com/vectors/checkmark-vector-icon-put-a-green-symbol-on-the-white-background-vector-id1151872207?k=20&m=1151872207&s=170667a&w=0&h=NzKOi1fjRT-jYlohJ7a15LRWHC3wK0LjTu1fi9yOQYQ=";
+
+
+}
+function afterr4(){
+  document.getElementById('image4').src="https://media.istockphoto.com/vectors/checkmark-vector-icon-put-a-green-symbol-on-the-white-background-vector-id1151872207?k=20&m=1151872207&s=170667a&w=0&h=NzKOi1fjRT-jYlohJ7a15LRWHC3wK0LjTu1fi9yOQYQ=";
+
+
+}
+function afterr5(){
+  document.getElementById('image5').src="https://media.istockphoto.com/vectors/checkmark-vector-icon-put-a-green-symbol-on-the-white-background-vector-id1151872207?k=20&m=1151872207&s=170667a&w=0&h=NzKOi1fjRT-jYlohJ7a15LRWHC3wK0LjTu1fi9yOQYQ=";
+
+
+}
